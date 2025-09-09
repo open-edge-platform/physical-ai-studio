20de60fa1b14c4fb8fe9083b67205e4f05ba3d2c
Vladislav Sovrasov <vladislav.sovrasov@intel.com>
2025-09-09 17:47:58 +0200

    Trim action lightning module

--- lib/src/action_trainer/policies/base/base_lightning_module.py
+++ lib/src/action_trainer/policies/base/base_lightning_module.py
@@ -20,11 +20,6 @@ class ActionTrainerModule(pl.LightningModule, ABC):
         self.save_hyperparameters()
 
         self.model: nn.Module
-        self.loss: nn.Module
-        self.callbacks: list[Callback]
-
-        self._input_image_sizes: list[tuple[int, int]] | None = None
-        self._input_state_shape: tuple[int] | None = None
 
     def forward(self, batch: TensorDict, *args, **kwargs) -> TensorDict:
         """Perform forward pass of the policy.
@@ -39,31 +34,4 @@ class ActionTrainerModule(pl.LightningModule, ABC):
             TensorDict: Processed batch with model predictions
         """
         del args, kwargs
-        processed_batch = self._preprocess_observation(batch)
-        return self.model(processed_batch)
-
-    @abstractmethod
-    def _preprocess_observation(batch: TensorDict) -> TensorDict:
-        """Preprocess the input observation batch before passing it to the torch model."""
-        raise NotImplementedError
-
-    def test_step(self, batch: TensorDict, batch_idx: int, *args, **kwargs) -> STEP_OUTPUT:
-        """Perform test step.
-
-        This method is called during the test stage of training. It calls
-        the model's forward method to ensure consistency with exported model behavior,
-        then merges the predictions into the batch for post-processing.
-
-        Args:
-            batch (TensorDict): Input batch
-            batch_idx (int): Index of the batch
-            *args: Additional positional arguments (unused)
-            **kwargs: Additional keyword arguments (unused)
-
-        Returns:
-            STEP_OUTPUT: Updated batch with model predictions
-        """
-        del args, kwargs, batch_idx  # These variables are not used.
-        processed_batch = self._preprocess_observation(batch)
-        predictions = self.model(processed_batch)
-        return batch.update(**predictions._asdict())
+        return self.model(batch)
