ac524068e94714f29d67f5b62202b388a9a1a3e4
Ronald Hecker <ronald.hecker@intel.com>
2025-09-14 07:37:19 +0200

    Use tabs for dataset wip

--- app/ui/src/routes/datasets/index.tsx
+++ app/ui/src/routes/datasets/index.tsx
@@ -1,7 +1,7 @@
 import { $api } from '../../api/client';
 import { ErrorMessage } from '../../components/error-page/error-page';
 import { useProject } from '../projects/project.provider';
-import { View, Text, Flex, Divider, Well } from '@geti/ui'
+import { View, Text, Flex, Divider, Well, Tabs, TabList, TabPanels, Item } from '@geti/ui'
 import { DatasetViewer } from './dataset-viewer';
 import { DatasetProvider } from './dataset.provider';
 
@@ -10,24 +10,30 @@ export const Index = () => {
     const dataset = project.datasets[0];
     return (
         <Flex direction={'column'} height={'100%'}>
-            <View>
-                {project.datasets.map((dataset) => (
-                    <Text key={dataset}>{dataset}</Text>
-                ))}
-            </View>
-            <Flex flex={1}>
-                <Well flex={1}>
-                    {dataset === undefined
-                        ? <Text>No datasets yet...</Text>
-                        : (
-                            <DatasetProvider project_id={project.id} repo_id={dataset}>
-                                <DatasetViewer />
-                            </DatasetProvider>
-                            )
-                    }
-                </Well>
-            </Flex>
+            <Tabs>
+                <TabList>
+                    <Item key="Test 1">Test</Item>
+                    <Item key="Test 2">Test</Item>
+                </TabList>
 
+                <TabPanels>
+                    <Item key="Test 1">
+                        <Well flex={1}>
+                            {dataset === undefined
+                                ? <Text>No datasets yet...</Text>
+                                : (
+                                    <DatasetProvider project_id={project.id} repo_id={dataset}>
+                                        <DatasetViewer />
+                                    </DatasetProvider>
+                                    )
+                            }
+                        </Well>
+                  
+                    </Item>
+
+                </TabPanels>
+
+            </Tabs>
         </Flex>
     )
 };
