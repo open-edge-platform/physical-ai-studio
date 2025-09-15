40045b8e344cd423fc8b5ed377b0432d432860dd
Ronald Hecker <ronald.hecker@intel.com>
2025-09-15 10:59:57 +0200

    Render actions in rechart chart

--- app/ui/src/routes/datasets/episode-viewer.tsx
+++ app/ui/src/routes/datasets/episode-viewer.tsx
@@ -1,14 +1,25 @@
 import { View, Text, Flex, Divider, Well, ListBox, Item, ListView, Selection } from '@geti/ui'
 import { SchemaEpisode } from "../../api/openapi-spec"
+import EpisodeChart from '../../components/episode-chart/episode-chart'
 
 interface EpisodeViewerProps {
     episodeIndex: number
     episode: SchemaEpisode
 }
+
+const joints = [
+    "shoulder_pan", 
+    "shoulder_lift",
+    "elbow_flex",
+    "wrist_flex",
+    "wrist_roll",
+    "gripper"
+];
+
 export const EpisodeViewer = ({episodeIndex, episode}: EpisodeViewerProps) => {
     return (
         <Flex>
-            <Text>Viewer goes here for episode {episodeIndex}</Text>
+            <EpisodeChart actions={episode.actions} joints={joints} fps={episode.fps}/>
         </Flex>
     )
 
