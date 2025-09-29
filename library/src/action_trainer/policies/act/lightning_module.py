f62ebdafac7e87812fbd8bf436093d52b30b76bf
Samet Akcay <samet.akcay@intel.com>
2025-09-29 12:42:07 +0100

    D100

--- library/src/action_trainer/policies/act/lightning_module.py
+++ library/src/action_trainer/policies/act/lightning_module.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2025-2026 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+"""ACT policy implementation via Lightning module."""
 
 from action_trainer.policies.act.torch_model import ACTModel
 from action_trainer.policies.base.base_lightning_module import TrainerModule
