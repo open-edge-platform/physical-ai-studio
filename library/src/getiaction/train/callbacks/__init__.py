0bffe24e362029194e8559191c3114ed6ef1e34b
samet-akcay <samet.akcay@intel.com>
2025-10-14 15:22:46 +0100

    refactor: Remove PolicyDatasetInteraction callback and related imports

--- library/src/getiaction/train/callbacks/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (C) 2025 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-"""Training callbacks for Lightning integration."""
-
-from getiaction.train.callbacks.policy_dataset_interaction import PolicyDatasetInteraction
-
-__all__ = ["PolicyDatasetInteraction"]
