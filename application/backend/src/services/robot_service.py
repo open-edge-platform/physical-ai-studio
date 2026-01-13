01f68d61209a82f50c8f009f24ae2766aff45a51
Mark Redeman <mark.s.redeman@intel.com>
2026-01-13 12:29:56 +0100

    feat(backend): Use project base repo for project robot

--- application/backend/src/services/robot_service.py
+++ application/backend/src/services/robot_service.py
@@ -10,39 +10,39 @@ class RobotService:
     @staticmethod
     async def get_robot_list(project_id: UUID) -> list[Robot]:
         async with get_async_db_session_ctx() as session:
-            repo = ProjectRobotRepository(session)
-            return await repo.get_all(project_id)
+            repo = ProjectRobotRepository(session, project_id)
+            return await repo.get_all()
 
     @staticmethod
     async def get_robot_by_id(project_id: UUID, robot_id: UUID) -> Robot:
         async with get_async_db_session_ctx() as session:
-            repo = ProjectRobotRepository(session)
-            robot = await repo.get_by_id(project_id, robot_id)
+            repo = ProjectRobotRepository(session, project_id)
+            robot = await repo.get_by_id(robot_id)
 
             if robot is None:
-                raise ResourceNotFoundError(ResourceType.ROBOT, str(project_id))
+                raise ResourceNotFoundError(ResourceType.ROBOT, project_id)
 
             return robot
 
     @staticmethod
     async def create_robot(project_id: UUID, robot: Robot) -> Robot:
         async with get_async_db_session_ctx() as session:
-            repo = ProjectRobotRepository(session)
-            return await repo.save(project_id, robot)
+            repo = ProjectRobotRepository(session, project_id)
+            return await repo.save(robot)
 
     @staticmethod
     async def update_robot(project_id: UUID, robot: Robot) -> Robot:
         async with get_async_db_session_ctx() as session:
-            repo = ProjectRobotRepository(session)
-            return await repo.update(project_id, robot.id, robot)
+            repo = ProjectRobotRepository(session, project_id)
+            return await repo.update(robot, partial_update=robot.model_dump(exclude={"id"}))
 
     @staticmethod
     async def delete_robot(project_id: UUID, robot_id: UUID) -> None:
         async with get_async_db_session_ctx() as session:
-            repo = ProjectRobotRepository(session)
+            repo = ProjectRobotRepository(session, project_id)
 
-            robot = await repo.get_by_id(project_id, robot_id)
+            robot = await repo.get_by_id(robot_id)
             if robot is None:
                 raise ResourceNotFoundError(ResourceType.ROBOT, str(robot_id))
 
-            await repo.delete_by_id(project_id, robot_id)
+            await repo.delete_by_id(robot_id)
