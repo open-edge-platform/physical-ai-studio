7a48bf6b714784508325600791543aa675760ce3
Mark Redeman <markredeman@gmail.com>
2026-01-15 09:40:45 +0100

    fix(ui): Fix height issue in sub layouts

--- application/ui/src/routes/robots/layout.tsx
+++ application/ui/src/routes/robots/layout.tsx
@@ -1,79 +1,15 @@
 import { Suspense } from 'react';
 
-import { Flex, Grid, Item, Loading, minmax, TabList, Tabs, View } from '@geti/ui';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Flex, Grid, Loading, minmax, View } from '@geti/ui';
+import { Outlet } from 'react-router-dom';
 
-import { useProjectId } from '../../features/projects/use-project';
 import { RobotsList } from '../../features/robots/robots-list';
-import { paths } from '../../router';
 
 const CenteredLoading = () => {
     return (
         <Flex width='100%' height='100%' alignItems={'center'} justifyContent={'center'}>
             <Loading mode='inline' />
         </Flex>
-    );
-};
-
-const Header = ({ project_id }: { project_id: string }) => {
-    return (
-        <View backgroundColor={'gray-200'} gridArea={'header'}>
-            <Flex height='100%' alignItems={'center'} marginX='1rem' gap='size-200'>
-                <TabList height={'100%'} width='100%'>
-                    <Item
-                        textValue='Robot configuration'
-                        key={'robots'}
-                        href={paths.project.robots.index({ project_id })}
-                    >
-                        <Flex alignItems='center' gap='size-100'>
-                            Robot arms
-                        </Flex>
-                    </Item>
-                    <Item textValue='Cameras' key={'cameras'} href={paths.project.cameras.index({ project_id })}>
-                        <Flex alignItems='center' gap='size-100'>
-                            Cameras
-                        </Flex>
-                    </Item>
-                    <Item
-                        textValue='Tele operation controller'
-                        key={'teleoperators'}
-                        href={paths.project.cameras.index({ project_id })}
-                    >
-                        <Flex alignItems='center' gap='size-100'>
-                            Tele operation controller
-                        </Flex>
-                    </Item>
-                    <Item
-                        textValue='Environments'
-                        key={'environments'}
-                        href={paths.project.environments.index({ project_id })}
-                    >
-                        <Flex alignItems='center' gap='size-100'>
-                            Environments
-                        </Flex>
-                    </Item>
-                </TabList>
-            </Flex>
-        </View>
-    );
-};
-
-export const RobotTabNavigation = () => {
-    const { project_id } = useProjectId();
-
-    const { pathname } = useLocation();
-
-    return (
-        <Tabs
-            aria-label='Header navigation'
-            selectedKey={
-                pathname.includes('cameras') ? 'cameras' : pathname.includes('environments') ? 'environments' : 'robots'
-            }
-            width='100%'
-            disabledKeys={['teleoperators', 'environments']}
-        >
-            <Header project_id={project_id} />
-        </Tabs>
     );
 };
 
