7751715b4a3d91a3c19d9a1abd202d1959d49eb2
Ronald Hecker <ronald.hecker@intel.com>
2026-02-20 14:42:04 +0100

    Boyscout the dataset creation not working

--- application/backend/src/repositories/mappers/dataset_mapper.py
+++ application/backend/src/repositories/mappers/dataset_mapper.py
@@ -7,16 +7,17 @@ class DatasetMapper(IBaseMapper):
     """Mapper for Dataset schema entity <-> DB entity conversions."""
 
     @staticmethod
-    def to_schema(dataset: Dataset) -> DatasetDB:
+    def to_schema(db_schema: Dataset) -> DatasetDB:
         """Convert Dataset schema to db model."""
         return DatasetDB(
-            id=str(dataset.id),
-            name=dataset.name,
-            path=dataset.path,
-            project_id=str(dataset.project_id),
+            id=str(db_schema.id),
+            name=db_schema.name,
+            path=db_schema.path,
+            project_id=str(db_schema.project_id),
+            environment_id=str(db_schema.environment_id),
         )
 
     @staticmethod
-    def from_schema(dataset_db: DatasetDB) -> Dataset:
+    def from_schema(model: DatasetDB) -> Dataset:
         """Convert Dataset db entity to schema."""
-        return Dataset.model_validate(dataset_db, from_attributes=True)
+        return Dataset.model_validate(model, from_attributes=True)
