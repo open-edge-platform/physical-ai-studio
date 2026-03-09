3f015f5d4ff699b6648089a4e700c762b4dfc8b4
Ronald Hecker <ronald.hecker@intel.com>
2026-03-09 12:20:08 +0100

    Extract integration between inference and environment from inference worker
    This should simplify the worker and make testing easier.
    Added WIP tests that use hardware in the loop.

---
+++ application/backend/src/workers/inference/inference_environment_integration.py
@@ -0,0 +1,119 @@
+from schemas.environment import EnvironmentWithRelations
+import asyncio
+import base64
+from frame_source.video_capture_base import VideoCaptureBase
+
+import cv2
+import numpy as np
+import torch
+from loguru import logger
+from physicalai.data import Observation
+
+from robots.robot_client import RobotClient
+from robots.robot_client_factory import RobotClientFactory
+from workers.camera_worker import create_frames_source_from_camera
+
+class CameraFrameProcessor:
+    @staticmethod
+    def process(frame: np.ndarray) -> np.ndarray:
+        """Post process camera frame."""
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+
+class InferenceEnvironmentIntegration:
+    environment: EnvironmentWithRelations
+    robot_client_factory: RobotClientFactory
+    action_keys: list[str] = []
+    camera_keys: list[str] = []
+    follower: RobotClient | None = None
+    cameras: dict[str, VideoCaptureBase] | None = None
+
+    def __init__(self, environment: EnvironmentWithRelations, robot_client_factory: RobotClientFactory):
+        self.environment = environment
+        self.robot_client_factory = robot_client_factory
+
+    async def setup(self) -> None:
+        robot = self.environment.robots[0]  # TODO: Handle multiple robots?
+
+        self.follower = await self.robot_client_factory.build(robot.robot)
+        self.action_keys = self.follower.features()
+
+        self.cameras = {str(camera.id): create_frames_source_from_camera(camera) for camera in self.environment.cameras}
+        logger.info(self.cameras)
+
+        for camera in self.cameras.values():
+            # camera.attach_processor(CameraFrameProcessor()) # TODO Not working. Fix in framesource
+            camera.connect()
+            camera.start_async()
+
+        await asyncio.sleep(1)  # sleep for camera warmup. TODO: Refactor start_async to proper camera wrapper
+        await self.follower.connect()
+
+    async def get_observation(self) -> dict | None:
+        if self.follower and self.cameras:
+            observation = (await self.follower.read_state())["state"]
+            for camera_id, camera in self.cameras.items():
+                _success, camera_frame = camera.get_latest_frame()  # HWC
+                if camera_frame is None:
+                    raise Exception("Camera frame is None")
+                processed_frame = CameraFrameProcessor.process(camera_frame)
+                observation[camera_id] = processed_frame
+
+            return observation
+
+    def format_observation_for_reporting(self, observation: dict, timestamp: float) -> dict:
+        # Discard batch for both actions and state
+        # TODO: Use camera id  for reporting.
+
+        if self.cameras:
+            camera_images = {
+                camera: self._base_64_encode_observation(cv2.cvtColor(observation[camera], cv2.COLOR_RGB2BGR))
+                for camera in self.cameras
+            }
+
+        return {
+            "state": {key: observation[key] for key in self.action_keys},
+            "actions": None,
+            "cameras": camera_images,
+            "timestamp": timestamp,
+        }
+
+    def format_model_input_observation(self, raw_observation: dict) -> Observation:
+        observation = self._to_lerobot_observations(raw_observation)
+        state = (
+            torch.tensor([value for key, value in observation.items() if key in self.action_keys]).unsqueeze(0).float()
+        )
+        images: dict = {}
+        for camera in self.environment.cameras:
+            camera_name = camera.name.lower()
+            # change image to 0..1 and swap R & B channels.
+            images[camera_name] = torch.from_numpy(observation[camera_name])
+            images[camera_name] = images[camera_name].float() / 255
+            images[camera_name] = images[camera_name].permute(2, 0, 1).contiguous()
+            images[camera_name] = images[camera_name].unsqueeze(0)
+
+        return Observation(
+            state=state,
+            images=images,
+        )
+
+    def _base_64_encode_observation(self, observation: np.ndarray | None) -> str:
+        if observation is None:
+            return ""
+        _, imagebytes = cv2.imencode(".jpg", observation)
+        return base64.b64encode(imagebytes).decode()
+
+    def _to_lerobot_observations(self, observations: dict) -> dict:
+        """Remap camera observations from camera ID keys to lowercase camera name keys."""
+        lerobot_observations = dict(observations)
+        for camera in self.environment.cameras:
+            lerobot_observations[camera.name.lower()] = lerobot_observations.pop(str(camera.id))
+        return lerobot_observations
+
+    async def teardown(self) -> None:
+        if self.follower:
+            await self.follower.disconnect()
+        if self.cameras:
+            for camera in self.cameras.values():
+                # camera.attach_processor(CameraFrameProcessor()) # TODO Not working. Fix in framesource
+                camera.stop()
---
+++ application/backend/tests/workers/inference/test_inference_environment_integration.py
@@ -0,0 +1,111 @@
+import torch
+import asyncio
+from workers.inference.inference_environment_integration import InferenceEnvironmentIntegration
+from uuid import UUID
+import datetime
+from schemas.environment import EnvironmentWithRelations
+from robots.robot_client_factory import RobotClientFactory
+import pytest
+from settings import get_settings
+
+from utils.serial_robot_tools import RobotConnectionManager
+from services.robot_calibration_service import RobotCalibrationService
+
+test_environment = {
+    'id': UUID('7656679b-25fe-4af5-a19d-73e7df16f384'),
+    'created_at': datetime.datetime(2026, 3, 3, 9, 36, 13),
+    'updated_at': datetime.datetime(2026, 3, 3, 9, 36, 13),
+    'name': 'Home Setup',
+    'robots': [
+        {
+            'robot': {
+                'id': UUID('c3f3f886-8813-4b3b-ba48-165cdaa39995'),
+                'created_at': datetime.datetime(2026, 3, 3, 9, 35, 9),
+                'updated_at': datetime.datetime(2026, 3, 3, 9, 35, 9),
+                'name': 'Khaos',
+                'connection_string': '',
+                'serial_number': '5AA9017083',
+                'type': 'SO101_Follower',
+                'active_calibration_id': UUID('877e5a03-47a6-4383-b15f-807259cd9691')
+            },
+            'tele_operator': {
+                'type': 'robot',
+                'robot_id': UUID('9da8143e-ea83-4811-88a8-5b4b02ee234d'),
+                'robot': {
+                    'id': UUID('9da8143e-ea83-4811-88a8-5b4b02ee234d'),
+                    'created_at': datetime.datetime(2026, 3, 3, 9, 35, 19),
+                    'updated_at': datetime.datetime(2026, 3, 3, 9, 35, 19),
+                    'name': 'Khronos',
+                    'connection_string': '',
+                    'serial_number': '5A7A016060',
+                    'type': 'SO101_Leader',
+                    'active_calibration_id': UUID('40399827-95bd-4151-bc20-893a5f51db8b')
+                }
+            }
+        }
+    ], 'cameras': [
+        {
+            'id': '3ed60255-04ae-407b-8e2c-c3281847a4e0',
+            'driver': 'usb_camera',
+            'created_at': datetime.datetime(2026, 3, 3, 9, 35, 53),
+            'updated_at': datetime.datetime(2026, 3, 3, 9, 35, 53),
+            'name': 'grabber',
+            'fingerprint': '/dev/video0:0',
+            'hardware_name': 'Innomaker-U20CAM-1080p-S1: Inno',
+            'payload': {'width': 640, 'height': 480, 'fps': 30, 'exposure': None, 'gain': None}
+        },
+        {
+            'id': '4629e172-2aa7-4fde-86b1-e19eb1d210ff',
+            'driver': 'usb_camera',
+            'created_at': datetime.datetime(2026, 3, 3, 9, 35, 46),
+            'updated_at': datetime.datetime(2026, 3, 3, 9, 35, 46),
+            'name': 'front',
+            'fingerprint': '/dev/video6:6',
+            'hardware_name': 'Intel(R) RealSense(TM) Depth Ca',
+            'payload': {'width': 640, 'height': 480, 'fps': 30, 'exposure': None, 'gain': None}
+        }
+    ]
+}
+
+
+@pytest.fixture
+def inference_environment_integration():
+
+    robot_manager = RobotConnectionManager()
+    asyncio.run(robot_manager.find_robots())
+    settings = get_settings()
+    calibration_service = RobotCalibrationService(robot_manager, settings)
+    factory = RobotClientFactory(robot_manager, calibration_service)
+    environment = EnvironmentWithRelations.model_validate(test_environment)
+
+    subject = InferenceEnvironmentIntegration(environment, factory)
+    asyncio.run(subject.setup())
+    yield subject
+    asyncio.run(subject.teardown())
+
+class TestInferenceEnvironmentIntegration:
+    # TODO: Hardware in the loop. FIX
+    def test_get_observation(self, inference_environment_integration: InferenceEnvironmentIntegration):
+        observation = asyncio.run(inference_environment_integration.get_observation())
+        assert observation is not None
+        assert "shoulder_pan.pos" in observation
+        assert "3ed60255-04ae-407b-8e2c-c3281847a4e0" in observation  #camera id 1
+        assert "4629e172-2aa7-4fde-86b1-e19eb1d210ff" in observation  #camera id 2
+
+    def test_transform_observation_to_model_input(self, inference_environment_integration: InferenceEnvironmentIntegration):
+        observation = asyncio.run(inference_environment_integration.get_observation())
+        assert observation is not None
+        phy_ai_obs = inference_environment_integration.format_model_input_observation(observation)
+        assert phy_ai_obs.state is not None
+        assert phy_ai_obs.state.shape == torch.Size([1, 6])
+        assert phy_ai_obs.images is not None
+        assert "front" in phy_ai_obs.images
+        assert "grabber" in phy_ai_obs.images
+
+    def test_transform_observation_to_report_to_ui(self, inference_environment_integration: InferenceEnvironmentIntegration):
+        observation = asyncio.run(inference_environment_integration.get_observation())
+        assert observation is not None
+        report_obs = inference_environment_integration.format_observation_for_reporting(observation, 0)
+        assert "shoulder_pan.pos" in report_obs["state"]
+        assert "3ed60255-04ae-407b-8e2c-c3281847a4e0" in report_obs["cameras"]  #camera id 1
+        assert "4629e172-2aa7-4fde-86b1-e19eb1d210ff" in report_obs["cameras"]  #camera id 2
