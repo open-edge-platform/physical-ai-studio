41fcaca9c2cc28ba0224f5035d91f96869812c27
Mark Redeman <markredeman@gmail.com>
2026-03-27 12:19:10 +0100

    chore: move RobotControlProivder higher
    This is so that we can keep track of draft episodes and show this in the header

--- application/ui/src/routes/datasets/record/index.tsx
+++ application/ui/src/routes/datasets/record/index.tsx
@@ -36,44 +36,44 @@ const RecordingPage = () => {
     );
 
     return (
-        <Grid
-            areas={['header', 'content']}
-            UNSAFE_style={{
-                gridTemplateRows: 'var(--spectrum-global-dimension-size-800, 4rem) auto',
-            }}
-            minHeight={0}
-            height={'100%'}
-        >
-            <View backgroundColor={'gray-300'} gridArea={'header'}>
-                <Flex height='100%' alignItems={'center'} marginX='1rem' gap='size-200'>
-                    <Link
-                        href={paths.project.datasets.show({ project_id, dataset_id })}
-                        isQuiet
-                        variant='overBackground'
-                    >
-                        <Flex marginEnd='size-200' direction='row' gap='size-200' alignItems={'center'}>
-                            <Icon>
-                                <ChevronLeft />
-                            </Icon>
-                            <Flex direction={'column'}>
-                                <Text UNSAFE_className={classes.headerText}>Adding Episode</Text>
-                                <Text UNSAFE_className={classes.subHeaderText}>
-                                    {environment.name} | {dataset.default_task}
-                                </Text>
+        <RobotControlProvider environment={environment} dataset={dataset} onError={ToastQueue.negative}>
+            <Grid
+                areas={['header', 'content']}
+                UNSAFE_style={{
+                    gridTemplateRows: 'var(--spectrum-global-dimension-size-800, 4rem) auto',
+                }}
+                minHeight={0}
+                height={'100%'}
+            >
+                <View backgroundColor={'gray-300'} gridArea={'header'}>
+                    <Flex height='100%' alignItems={'center'} marginX='1rem' gap='size-200'>
+                        <Link
+                            href={paths.project.datasets.show({ project_id, dataset_id })}
+                            isQuiet
+                            variant='overBackground'
+                        >
+                            <Flex marginEnd='size-200' direction='row' gap='size-200' alignItems={'center'}>
+                                <Icon>
+                                    <ChevronLeft />
+                                </Icon>
+                                <Flex direction={'column'}>
+                                    <Text UNSAFE_className={classes.headerText}>Adding Episode</Text>
+                                    <Text UNSAFE_className={classes.subHeaderText}>
+                                        {environment.name} | {dataset.default_task}
+                                    </Text>
+                                </Flex>
                             </Flex>
-                        </Flex>
-                    </Link>
-                </Flex>
-            </View>
-
-            <View gridArea={'content'} maxHeight={'100vh'} minHeight={0} height='100%'>
-                <View padding='size-200' height='100%'>
-                    <RobotControlProvider environment={environment} dataset={dataset} onError={ToastQueue.negative}>
-                        <RecordingViewer />
-                    </RobotControlProvider>
+                        </Link>
+                    </Flex>
                 </View>
-            </View>
-        </Grid>
+
+                <View gridArea={'content'} maxHeight={'100vh'} minHeight={0} height='100%'>
+                    <View padding='size-200' height='100%'>
+                        <RecordingViewer />
+                    </View>
+                </View>
+            </Grid>
+        </RobotControlProvider>
     );
 };
 
