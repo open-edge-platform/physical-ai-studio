48b59eb85a7a80a31bc119f20b846b954f04d987
Mark Redeman <markredeman@gmail.com>
2026-03-30 08:56:35 +0200

    chore: replace `@geti/config` with direct config

--- application/ui/eslint.config.js
+++ application/ui/eslint.config.js
@@ -6,8 +6,9 @@ import { fileURLToPath } from 'node:url';
 
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import sharedEslintConfig from '@geti/config/lint';
 import reactCompiler from 'eslint-plugin-react-compiler';
+
+import sharedEslintConfig from './eslint.shared.config.js';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
