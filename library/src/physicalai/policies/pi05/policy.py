a3cf8deeedc5c643536804912ec7ac06ebe727fc
Eugene Liu <eugene.liu@intel.com>
2026-03-30 05:54:19 +0100

    feat(pi05): update hyperparameter saving to include pretrained model path

--- library/src/physicalai/policies/pi05/policy.py
+++ library/src/physicalai/policies/pi05/policy.py
@@ -185,7 +185,7 @@ class Pi05(ExportablePolicyMixin, Policy):
                 scheduler_decay_lr=scheduler_decay_lr,
             )
 
-        self.save_hyperparameters(ignore=["config"])
+        self.save_hyperparameters(ignore=["config", "pretrained_name_or_path"])
         self.hparams["config"] = self.config.to_dict()
 
         self.model: Pi05Model | None = None
