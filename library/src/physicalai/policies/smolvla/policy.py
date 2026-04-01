46c5c3e95bb1bc6ba92481e8b4884b95eb102407
Vlad Sovrasov <sovrasov.vlad@gmail.com>
2026-04-01 23:27:50 +0900

    Fit export backends lists to actual state

--- library/src/physicalai/policies/smolvla/policy.py
+++ library/src/physicalai/policies/smolvla/policy.py
@@ -429,4 +429,4 @@ class SmolVLA(ExportablePolicyMixin, Policy):
         Returns:
             list[str | ExportBackend]: A list of supported export backends.
         """
-        return [ExportBackend.TORCH, ExportBackend.OPENVINO, ExportBackend.ONNX]
+        return [ExportBackend.TORCH, ExportBackend.OPENVINO]
