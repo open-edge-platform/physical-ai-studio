d066e1b829dca31ca25788239208c673b5d1cf77
Max Xiang <xiangxiang.ma@intel.com>
2026-04-13 11:32:32 +0200

    robot_adapter.py -> so101/adapter.py

--- application/backend/src/robots/robot_client_factory.py
+++ application/backend/src/robots/robot_client_factory.py
@@ -1,8 +1,8 @@
 from physicalai.robot.so101 import SO101, SO101Calibration
 
 from exceptions import ResourceNotFoundError, ResourceType
-from robots.robot_adapter import SO101Adapter
 from robots.robot_client import RobotClient
+from robots.so101.adapter import SO101Adapter
 from robots.widowxai.trossen_widowx_ai_follower import TrossenWidowXAIFollower
 from robots.widowxai.trossen_widowx_ai_leader import TrossenWidowXAILeader
 from schemas.calibration import Calibration
