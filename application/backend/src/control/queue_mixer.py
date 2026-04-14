1520c54734a1c020ae99413ec9eee1913b5073d1
Ronald Hecker <ronald.hecker@intel.com>
2026-04-14 11:48:35 +0200

    Improve comments for queue mixer

--- application/backend/src/control/queue_mixer.py
+++ application/backend/src/control/queue_mixer.py
@@ -17,23 +17,34 @@ class QueueMixer:
         self.lerp_duration = lerp_duration
 
     def add(self, row: np.ndarray, offset: int = 0) -> None:
+        """
+        Add a row to be merged at specific offset.
+
+        This offset is meant to compensate the time between inference call and inference result.
+
+        Args:
+             row: list of actions
+             offset: number of frames that will be removed from list of actions
+
+        """
         if self.queue is None or len(self.queue) <= self.index:
-            # No queue? Plain set queue without offset
+            # No existing queue? Set queue without offset
             self.queue = row
         else:
-            # Already have a queue merge new queue
-            # self.queue = self.merge_queues(self.queue, row[offset:])
-            remaining_queue = self.queue[self.index :]
+            # Take remaining part of existing queue.
+            remaining_queue = self.queue[self.index:]
+
+            # Remove part of queue that has already passed during inference.
             upcoming_queue = row[offset:]
+
+            # Build an array for factors from 1..0 and 0..1 for the remaining and upcoming queue
             n_remaining_queue = len(remaining_queue)
             lerp_duration = min(n_remaining_queue, self.lerp_duration)
             factors = np.maximum(1 - np.arange(0, n_remaining_queue) * 1 / lerp_duration, 0)
-            # t = np.clip(np.arange(0, n_remaining_queue) / lerp_duration, 0, 1)
-            # factors = 1 - (3 * t**2 - 2 * t**3)
-            # t = np.arange(0, n_remaining_queue) / lerp_duration
-            # factors = 1 - 1 / (1 + np.exp(-(t - 0.5) * 10))
             extra_dims = (1,) * (remaining_queue.ndim - 1)
             factors = factors.reshape(n_remaining_queue, *extra_dims)
+
+            # Merge queues with respective factors
             n_blend = min(n_remaining_queue, len(upcoming_queue))
             overlap = factors[:n_blend] * remaining_queue[:n_blend] + (1 - factors[:n_blend]) * upcoming_queue[:n_blend]
             self.queue = np.concatenate([overlap, upcoming_queue[n_blend:]], axis=0)
@@ -41,7 +52,6 @@ class QueueMixer:
         self.index = 0
 
     def clear(self) -> None:
-        """Clear mixer queue."""
         self.queue = None
 
     def pop(self) -> np.ndarray:
