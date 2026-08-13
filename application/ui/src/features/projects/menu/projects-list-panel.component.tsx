0f7faef983bb8c782fd9178dee3e8031db72464d
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-08-13 12:41:43 +0200

    use page links for different page navigations, remove projects list

--- application/ui/src/features/projects/menu/projects-list-panel.component.tsx
+++ application/ui/src/features/projects/menu/projects-list-panel.component.tsx
@@ -1,7 +1,5 @@
 import {
     ActionButton,
-    ActionMenu,
-    ButtonGroup,
     Content,
     Dialog,
     DialogTrigger,
@@ -9,65 +7,15 @@ import {
     Flex,
     Header,
     Heading,
-    Icon,
-    Item,
-    Link,
-    Menu,
     PhotoPlaceholder,
-    Text,
     View,
 } from '@geti-ui/ui';
-import { ChevronRightSmallLight } from '@geti-ui/ui/icons';
 
-import { $api } from '../../../api/client';
 import { paths } from '../../../router';
-import { NewProjectLink } from '../list/new-project-link.component';
-import { useProjectId } from './../use-project';
+import { useProject } from './../use-project';
+import { PanelLink } from './page-link/panel-link.component';
 
 import styles from './projects-list.module.css';
-
-interface Project {
-    name: string;
-    id?: string;
-}
-interface ProjectListProps {
-    menuWidth?: string;
-    projects: Project[];
-}
-
-export const ProjectsList = ({ menuWidth = '100%', projects }: ProjectListProps) => {
-    const deleteMutation = $api.useMutation('delete', '/api/projects/{project_id}', {
-        meta: {
-            invalidates: [['get', '/api/projects']],
-        },
-    });
-
-    return (
-        <Menu UNSAFE_className={styles.projectMenu} width={menuWidth} items={projects}>
-            {(item) => (
-                <Item key={item.id} textValue={item.name} href={paths.project.robots.index({ project_id: item.id! })}>
-                    <Text>
-                        <Flex justifyContent='space-between' alignItems='center' marginX={'size-200'}>
-                            {item.name}
-                            <ActionMenu
-                                isQuiet
-                                onAction={(key) => {
-                                    if (key === 'delete' && item.id) {
-                                        deleteMutation.mutate({
-                                            params: { path: { project_id: item.id } },
-                                        });
-                                    }
-                                }}
-                            >
-                                <Item>Delete</Item>
-                            </ActionMenu>
-                        </Flex>
-                    </Text>
-                </Item>
-            )}
-        </Menu>
-    );
-};
 
 interface SelectedProjectProps {
     name: string;
@@ -79,10 +27,7 @@ const SelectedProjectButton = ({ name }: SelectedProjectProps) => {
             aria-label={`Selected project ${name}`}
             isQuiet
             staticColor='white'
-            UNSAFE_style={{
-                paddingInline: 'var(--spectrum-global-dimension-size-200)',
-                paddingBlock: 'var(--spectrum-global-dimension-size-200)',
-            }}
+            UNSAFE_className={styles.selectedProjectButton}
         >
             <View margin={'size-50'}>{name}</View>
             <View margin='size-50'>
@@ -93,14 +38,7 @@ const SelectedProjectButton = ({ name }: SelectedProjectProps) => {
 };
 
 export const ProjectsListPanel = () => {
-    const { project_id } = useProjectId();
-    const { data: projects } = $api.useSuspenseQuery('get', '/api/projects');
-    const { data: project } = $api.useSuspenseQuery('get', '/api/projects/{project_id}', {
-        params: {
-            path: { project_id },
-        },
-    });
-    const otherProjects = projects.filter(({ id }) => id !== project_id);
+    const project = useProject();
 
     const selectedProjectName = project.name;
 
@@ -108,55 +46,28 @@ export const ProjectsListPanel = () => {
         <DialogTrigger type='popover' hideArrow>
             <SelectedProjectButton name={selectedProjectName} />
 
-            {(close) => {
-                return (
-                    <Dialog width={'size-4600'} UNSAFE_className={styles.dialog} onDismiss={close}>
-                        <Header>
-                            <Flex direction={'column'} justifyContent={'center'} width={'100%'} alignItems={'center'}>
-                                <PhotoPlaceholder
-                                    name={selectedProjectName}
-                                    indicator={selectedProjectName}
-                                    height={'size-1000'}
-                                    width={'size-1000'}
-                                />
-                                <Heading level={2} marginBottom={0}>
-                                    {selectedProjectName}
-                                </Heading>
-                            </Flex>
-                        </Header>
-                        <Content UNSAFE_className={styles.panelContent}>
-                            {otherProjects.length > 0 && (
-                                <>
-                                    <Divider size={'S'} marginY={'size-200'} />
-                                    <ProjectsList projects={otherProjects} />
-                                </>
-                            )}
-                            <Divider size={'S'} marginTop={'size-200'} />
+            <Dialog width={'size-4600'} UNSAFE_className={styles.dialog}>
+                <Header marginTop={'size-250'}>
+                    <Flex direction={'column'} justifyContent={'center'} width={'100%'} alignItems={'center'}>
+                        <PhotoPlaceholder
+                            name={selectedProjectName}
+                            indicator={selectedProjectName}
+                            height={'size-1000'}
+                            width={'size-1000'}
+                        />
+                        <Heading level={2} marginBottom={0}>
+                            {selectedProjectName}
+                        </Heading>
+                    </Flex>
+                </Header>
+                <Content UNSAFE_className={styles.panelContent}>
+                    <Divider size={'S'} marginTop={'size-200'} />
 
-                            <Link
-                                href={paths.openapi({})}
-                                UNSAFE_style={{ color: 'white', textDecoration: 'none' }}
-                                UNSAFE_className={styles.openApiLink}
-                            >
-                                <Flex alignItems={'center'} gap='size-100'>
-                                    <Icon>
-                                        <ChevronRightSmallLight fill='white' />
-                                    </Icon>
-                                    OpenAPI Spec
-                                </Flex>
-                            </Link>
-
-                            <Divider size={'S'} />
-
-                            <Divider size={'S'} marginBottom={'size-200'} />
-                        </Content>
-
-                        <ButtonGroup UNSAFE_className={styles.panelButtons}>
-                            <NewProjectLink className={styles.addProjectButton} />
-                        </ButtonGroup>
-                    </Dialog>
-                );
-            }}
+                    <PanelLink href={paths.projects.index({})}>Projects</PanelLink>
+                    <PanelLink href={paths.settings.index({})}>Settings</PanelLink>
+                    <PanelLink href={paths.openapi({})}>OpenAPI Spec</PanelLink>
+                </Content>
+            </Dialog>
         </DialogTrigger>
     );
 };
