5d68186a0902565052e1678612f8a09dff16ccf5
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-08-25 14:10:57 +0200

    use classes and clsx

--- application/ui/src/features/logs/jump-to-latest-button.tsx
+++ application/ui/src/features/logs/jump-to-latest-button.tsx
@@ -3,15 +3,18 @@
 
 import { ActionButton, Icon, Text } from '@geti-ui/ui';
 import { ChevronDownLight } from '@geti-ui/ui/icons';
+import { clsx } from 'clsx';
 
-import styles from './log-viewer.module.css';
+import classes from './log-viewer.module.css';
 
 export const JumpToLatestButton = ({ isVisible, onPress }: { isVisible: boolean; onPress: () => void }) => {
     return (
         <ActionButton
             onPress={onPress}
             excludeFromTabOrder={!isVisible}
-            UNSAFE_className={`${styles.jumpToLatest} ${isVisible ? '' : styles.jumpToLatestHidden}`}
+            UNSAFE_className={clsx(classes.jumpToLatest, {
+                [classes.jumpToLatestHidden]: !isVisible,
+            })}
         >
             <Text>Jump to latest</Text>
             <Icon>
