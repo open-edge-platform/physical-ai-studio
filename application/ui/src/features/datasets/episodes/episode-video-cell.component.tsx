e80dc8314231195e7cd9a8538124346823dfd0f5
Dawid Wesołowski <dawid.wesolowski@intel.com>
2026-09-01 17:09:36 +0200

    fix: load new episode video when episode has been changed

--- application/ui/src/features/datasets/episodes/episode-video-cell.component.tsx
+++ application/ui/src/features/datasets/episodes/episode-video-cell.component.tsx
@@ -5,29 +5,29 @@ import { SchemaEpisodeVideo } from '../../../api/openapi-spec';
 import { useFittedMediaSize } from '../../cameras/use-fitted-media-size';
 import { useEpisodeViewer } from './episode-viewer-provider.component';
 
-export const EpisodeVideoCell = ({
-    episodeVideo,
-    datasetId,
-}: {
-    episodeVideo: SchemaEpisodeVideo;
-    datasetId: string;
-}) => {
-    const { player } = useEpisodeViewer();
-    const url = fetchClient.PATH('/api/dataset/{dataset_id}/video/{video_path}', {
-        params: {
-            path: {
-                dataset_id: datasetId,
-                video_path: episodeVideo.path,
-            },
-        },
-    });
+export const EpisodeVideoCell = ({ videoId, datasetId }: { videoId: string; datasetId: string | undefined }) => {
+    const { player, episode } = useEpisodeViewer();
+
+    const episodeVideo: SchemaEpisodeVideo | undefined = episode.videos[videoId];
+
+    const url =
+        episodeVideo === undefined || datasetId === undefined
+            ? undefined
+            : fetchClient.PATH('/api/dataset/{dataset_id}/video/{video_path}', {
+                  params: {
+                      path: {
+                          dataset_id: datasetId,
+                          video_path: episodeVideo.path,
+                      },
+                  },
+              });
 
     const videoRef = useRef<HTMLVideoElement>(null);
 
     useEffect(() => {
         const video = videoRef.current;
-        const start = episodeVideo.start;
-        if (!video || !Number.isFinite(start)) return;
+        const start = episodeVideo?.start;
+        if (!video || start === undefined || !Number.isFinite(start)) return;
 
         video.currentTime = player.timeRef.current + start;
         if (player.isPlaying) {
@@ -35,19 +35,19 @@ export const EpisodeVideoCell = ({
         } else {
             video.pause();
         }
-    }, [player, episodeVideo.start, videoRef]);
+    }, [player, episodeVideo?.start, videoRef]);
 
     useEffect(() => {
         const video = videoRef.current;
-        const start = episodeVideo.start;
+        const start = episodeVideo?.start;
 
-        if (video && player.isSeeking && Number.isFinite(start)) {
+        if (video && player.isSeeking && start !== undefined && Number.isFinite(start)) {
             const interval = setInterval(() => {
                 video.currentTime = player.timeRef.current + start;
             }, 1000 / 60);
             return () => clearInterval(interval);
         }
-    }, [player, episodeVideo.start]);
+    }, [player, episodeVideo?.start]);
 
     const { containerRef, width, height } = useFittedMediaSize(
         videoRef.current?.videoWidth,
@@ -57,7 +57,7 @@ export const EpisodeVideoCell = ({
     /* eslint-disable jsx-a11y/media-has-caption */
     return (
         <div ref={containerRef} style={{ height: '100%', width: '100%' }}>
-            <video ref={videoRef} src={url} width={width} height={height} />
+            {url !== undefined && <video ref={videoRef} src={url} width={width} height={height} />}
         </div>
     );
 };
